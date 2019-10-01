ca112d75b4cd8d6f4dbb1cf8679c4f48b4056948
Tina Pollard <smithtina1203@aol.com>
2019-10-01 09:34:48 -0500

    adjust format

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
 function clickHandler() {
-    var name = document.getElementById('nameid');  
-    var email = document.getElementById('emailid'); 
-    var message = document.getElementById('msgid'); 
+    var name = document.getElementById('nameid');
+    var email = document.getElementById('emailid');
+    var message = document.getElementById('msgid');
 
-    if(name.value != "" && email.value != "" && message.value != "")
-    {
+    if (name.value != "" && email.value != "" && message.value != "") {
         alert("Thank you for your inquiry " + name.value + "!");
     }
-    
+
 }
